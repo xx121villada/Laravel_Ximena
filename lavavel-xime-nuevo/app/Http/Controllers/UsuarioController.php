536d02f8e9648c6d7e1a504ae8759421b232cd9a
xx121villada <x336562@gmail.com>
2024-10-01 14:19:39 -0500

    creacionRutaApi

---
+++ lavavel-xime-nuevo/app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    public function control(){
+        return ("Ximena, Mensaje por medio del controlador");
+    }
+}
